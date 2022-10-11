b9e500195a9a2c668305ffb0101310143fd1e7cb
David Cabello <david.cabello@dxc.com>
2022-10-11 11:13:51 -0500

    Added user mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useReducer, createContext } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home, Login, Profile } from './components';
+import { userData } from './config/mockData';
 export const AppContext = createContext({});
 import { reducer } from './providers/reducer';
 
 const App = () => {
   const [appContext, setAppContext] = useReducer(reducer, {
     products: [],
-    user: {}
+    user: userData
   });
   return (
     <AppContext.Provider value={{ appContext, setAppContext }}>
---
+++ src/config/mockData.js
@@ -0,0 +1,15 @@
+export const userData = {
+  userName: 'Arturo S.',
+  password: '',
+  fiscalName: '',
+  buyer: '',
+  registerDate: '',
+  rfc: '',
+  userID: '',
+  email: '',
+  cli: '',
+  payments: '',
+  profilePicture: '',
+  address: {},
+  paymentInfo: {}
+};
