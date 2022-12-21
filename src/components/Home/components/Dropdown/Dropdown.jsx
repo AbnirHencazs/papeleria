86cbb08591fb67f9b19ba4b9d6a59014ea8230ce
Brian Sánchez <brian.sanchez@inmediatum.com>
2022-12-21 13:58:46 -0600

    Expand Dropdown to use an extra menu

--- src/components/Home/components/Dropdown/Dropdown.jsx
+++ src/components/Home/components/Dropdown/Dropdown.jsx
@@ -5,10 +5,24 @@ import styles from './Dropdown.module.scss';
 const Dropdown = ({ title, subtitle, list }) => {
   const [dropdown, setDropdown] = useState(false);
   const [secDropdown, setSecDropdown] = useState({ name: '' });
+  const [thirdDropdown, setThirdDropdown] = useState({ name: '' });
   const ArrowIcon = () => <img src={arrow} alt="arrow" />;
   const showProducts = (event, item) => {
     event.stopPropagation();
+    if (item.name === secDropdown.name) {
+      setSecDropdown({ name: '' });
+      return;
+    }
+    setThirdDropdown({ name: '' });
     setSecDropdown({ name: item.name });
+  };
+  const showMoreProducts = (ev, product) => {
+    ev.stopPropagation();
+    if (product.name === thirdDropdown.name) {
+      setThirdDropdown({ name: '' });
+      return;
+    }
+    setThirdDropdown({ name: product.name });
   };
   return (
     <div className={styles.dropdown__container} onClick={() => setDropdown(!dropdown)}>
@@ -18,7 +32,7 @@ const Dropdown = ({ title, subtitle, list }) => {
       <ArrowIcon />
       {dropdown && (
         <div className={styles.dropdown__menu}>
-          {list.map((item) => (
+          {list?.map((item) => (
             <>
               <div
                 key={item.code}
@@ -30,9 +44,25 @@ const Dropdown = ({ title, subtitle, list }) => {
                 <div className={styles.dropdown__sec_menu}>
                   {item?.productList.map((product) =>
                     item.name === secDropdown.name ? (
-                      <div key={product.code * 2} className={styles.item_container}>
-                        {product.name}
-                      </div>
+                      <>
+                        <div
+                          key={product.code * 2}
+                          className={styles.item_container}
+                          onClick={(e) => showMoreProducts(e, product)}>
+                          {product.name}
+                        </div>
+                        {thirdDropdown.name && (
+                          <div className={styles.dropdown__sec_menu}>
+                            {product?.productList?.map((p) =>
+                              product.name === thirdDropdown.name ? (
+                                <div key={p.code} className={styles.item_container}>
+                                  {p.name}
+                                </div>
+                              ) : null
+                            )}
+                          </div>
+                        )}
+                      </>
                     ) : null
                   )}
                 </div>
